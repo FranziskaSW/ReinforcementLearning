791f2fc531b6ecae1d8a06a8a748affc726f7565
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-25 01:23:10 +0200

    act and learn works but performs very bad

--- policies/DQNetwork.py
+++ policies/DQNetwork.py
@@ -27,16 +27,17 @@ class DQNetwork():
         self.model.compile(loss=keras.losses.categorical_crossentropy,
                            optimizer='Adam',  # default learning rate = 0.01
                            metrics=['accuracy'])
+        self.act2idx = {'L': 0, 'R': 1, 'F': 2}
 
     def learn(self, batches):
         x = []
         y = []
-
-        for batch in batches:  # ((s_t, a_t) = q_t, r_t, s_(t+1))
-            # calculate the gradiend step: y_t = r_t + gamma * Q_(t+1)
+        for batch in batches:
             x.append(batch['s_t'])
-            b = batch['s_tp1'][np.newaxis, ..., np.newaxis]
-            y_t = batch['r_t'] + self.gamma * np.max(self.predict(b))
+            a_idx = self.act2idx[batch['a_t']]
+            vec = np.zeros(3)
+            vec[a_idx] = 1
+            y_t = (batch['r_t'] + self.gamma * np.max(self.predict(batch['s_tp1']))) * vec
             y.append(y_t)
 
         x = np.array(x)[..., np.newaxis]
@@ -44,30 +45,8 @@ class DQNetwork():
 
         self.model.fit(x, y, batch_size=self.batch_size, nb_epoch=1)
 
-        # for datapoint in batch:
-        #
-        #     # The error must be 0 on all actions except the one taken
-        #     t = list(self.predict(datapoint['source'])[0])
-        #     if datapoint['final']:
-        #         t[datapoint['action']] = datapoint['reward']
-        #     else:
-        #         t[datapoint['action']] = datapoint['reward'] + \
-        #                                  self.gamma * next_q_value
-        #
-        #     t_train.append(t)
-        #
-        #     # Prepare inputs and targets
-        # x_train = np.asarray(x_train).squeeze()
-        # t_train = np.asarray(t_train).squeeze()
-        #
-        # # Train the model for one epoch
-        # h = self.model.fit(x_train,
-        #                    t_train,
-        #                    batch_size=32,
-        #                    nb_epoch=1)
-
     def predict(self, state):
-        s = state[np.newaxis, ..., np.newaxis]
+        s = state[np.newaxis, ..., np.newaxis] # bring in right format
         q_values = self.model.predict(s, batch_size=1)
         return q_values
 
--- policies/policy_deepq.py
+++ policies/policy_deepq.py
@@ -10,48 +10,32 @@ EPSILON = 0.3
 ALPHA = 0.5
 GAMMA = 0.5
 DROPOUT_RATE = 0.1
-INPUT_SHAPE = (20, 20, 1)
+INPUT_SHAPE = (9, 9, 1)
 NUM_ACTIONS = 3
-BATCH_SIZE = 32
+BATCH_SIZE = 4
 
 class MyPolicy(bp.Policy):
     """
-    A policy which avoids collisions with obstacles and other snakes. It has an epsilon parameter which controls the
-    percentag of actions which are randomly chosen.
     """
 
     def cast_string_args(self, policy_args):
         policy_args['epsilon'] = float(policy_args['epsilon']) if 'epsilon' in policy_args else EPSILON
+        policy_args['batch_size'] = float(policy_args['batch_size']) if 'batch_size' in policy_args else BATCH_SIZE
         return policy_args
 
     def init_run(self):
         self.r_sum = 0
-        self.Q = DQN.DQNetwork(input_shape=(20, 20, 1), alpha=0.5, gamma=0.5,
-                               dropout_rate=0.1, num_actions=3, batch_size=32)
-        # self.alpha = ALPHA
-        # self.gamma = GAMMA
-        # self.dropout_rate = DROPOUT_RATE
-        # self.input_shape = INPUT_SHAPE
-        # self.num_actions = NUM_ACTIONS
-        # self.batch_size = BATCH_SIZE
+        self.Q = DQN.DQNetwork(input_shape=(9, 9, 1), alpha=0.5, gamma=0.5,
+                               dropout_rate=0.1, num_actions=3, batch_size=self.batch_size)
+        self.memory = []
+        self.act2idx = {'L': 0, 'R':1, 'F':2}
 
-        # self.model = Sequential()
-        # self.model.add(Conv2D(32, kernel_size=(3, 3),
-        #                       activation='relu',
-        #                       input_shape=self.input_shape))
-        # self.model.add(Conv2D(64, (3, 3), activation='relu'))
-        # self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        # self.model.add(Dropout(self.dropout_rate))
-        # self.model.add(Flatten())
-        # self.model.add(Dense(128, activation='relu'))
-        # self.model.add(Dropout(self.dropout_rate))
-        # self.model.add(Dense(self.num_actions, activation='softmax'))
-        #
-        # self.model.compile(loss=keras.losses.categorical_crossentropy,
-        #                    optimizer='Adam',  # default learning rate = 0.01
-        #                    metrics=['accuracy'])
 
     def learn(self, round, prev_state, prev_action, reward, new_state, too_slow):
+
+        if round >= 50:
+           random_batches = np.random.choice(self.memory, self.batch_size)
+           self.Q.learn(random_batches)
 
         try:
             if round % 100 == 0:
@@ -70,7 +54,12 @@ class MyPolicy(bp.Policy):
     def act(self, round, prev_state, prev_action, reward, new_state, too_slow):
 
         board, head = new_state
-        head_pos, direction = head
+        # head_pos, direction = head
+
+        if round >=2:  # update to memory from previous round (prev_state)
+            board_prev, head_prev = prev_state
+            state_before = board_prev
+            self.memory.append({'s_t': state_before, 'a_t': prev_action, 'r_t': reward, 's_tp1': board})
 
         if np.random.rand() < self.epsilon:
             return np.random.choice(bp.Policy.ACTIONS)
@@ -78,4 +67,4 @@ class MyPolicy(bp.Policy):
         else:
             q_values = self.Q.predict(board)
             idx = np.argmax(q_values)
-            return bp.Policy.ACTIONS[idx]
+            return bp.Policy.ACTIONS[idx]  # TODO: right now assume L, R, F as it is defined
