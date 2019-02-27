c6de5647a3e33b0cd45239384c81afdeb2f61c64
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-27 19:29:19 +0200

    sbatch

--- sbatch.sh
+++ sbatch.sh
@@ -9,6 +9,6 @@
 source /cs/labs/shais/dsgissin/apml_snake/bin/activate.csh
 module load tensorflow
 
-python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001)" -D 7000 -s 1000 -l "/logs/match_01.log" -r 0 -plt 0.01 -pat 0.005 -pit 15
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001)" -D 7000 -s 1000 -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_01.log" -r 0 -plt 0.01 -pat 0.005 -pit 15
 
-python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)" -D 7000 -s 1000 -l "/logs/match_02.log" -r 0 -plt 0.01 -pat 0.005 -pit 15
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)" -D 7000 -s 1000 -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_02.log" -r 0 -plt 0.01 -pat 0.005 -pit 15
