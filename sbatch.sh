2f088f513d072c86893f6debbf3323bc42842257
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-27 19:42:09 +0200

    sbatch

--- sbatch.sh
+++ sbatch.sh
@@ -9,8 +9,18 @@
 source /cs/labs/shais/dsgissin/apml_snake/bin/activate.csh
 module load tensorflow
 
-python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01)" -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
+    -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01)" \
+    -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60
 
-python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001)" -D 7000 -s 1000 -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_01.log" -r 0 -plt 0.11 -pat 0.015 -pit 60
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
+    -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001)" \
+    -D 7000 -s 1000 -r 0 -plt 0.11 -pat 0.015 -pit 60 \
+    -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_01.log" \
+    -o "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_01.out"
 
-python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)" -D 7000 -s 1000 -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_02.log" -r 0 -plt 0.01 -pat 0.005 -pit 60
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
+    -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)"
+    -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60 \
+    -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_02.log" \
+    -o "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_02.out"
