8cbdb9d15cdc49a4bcdcde108939990622bdb074
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-27 19:18:11 +0200

    upload

---
+++ sbatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=2
+#SBATCH --output=<YOUR_OUTPUT_PATH_HERE>
+#SBATCH --mem-per-cpu=500M
+#SBATCH --account=aml
+#SBATCH --constraint="sm"
+
+source /cs/labs/shais/dsgissin/apml_snake/bin/activate.csh
+module load tensorflow
+
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.001)" -D 5000 -s 1000 -l "/logs/match_01.log" -r 0 -plt 0.01 -pat 0.005 -pit 15
+
