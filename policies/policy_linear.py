89032ea673c460b20b654d8859998a09184b18dd
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-27 17:55:13 +0200

    linear works

--- policies/policy_linear.py
+++ policies/policy_linear.py
@@ -7,7 +7,7 @@ EPSILON = 0.05
 EPSILON_RATE = 0.9999
 GAMMA = 0.8
 LEARNING_RATE = 0.01
-FEATURE_NUM = len(['Food1', 'Food2', 'Food3', 'FieldEmpty'])
+FEATURE_NUM = 11  # len(['Food1', 'Food2', 'Food3', 'FieldEmpty'])
 
 class Linear(bp.Policy):
     """
@@ -25,7 +25,7 @@ class Linear(bp.Policy):
     def init_run(self):
         self.r_sum = 0
         weights = np.matrix(np.random.uniform(0, 1, FEATURE_NUM))
-        self.weights = weights / weights.sum()
+        self.weights = np.zeros((1, FEATURE_NUM)) # weights / weights.sum()
         self.features = np.zeros(FEATURE_NUM)
         self.last_actions = []  #
         self.last_qvalues = []
@@ -43,6 +43,10 @@ class Linear(bp.Policy):
         #delta = self.last_deltas[-1]
 
         #last_features = self.last_features
+
+
+        #self.epsilon = self.epsilon * EPSILON_RATE
+
         feature_mat = np.matrix(self.last_features)
         delta_mat = np.matrix(self.last_deltas)
 
@@ -66,59 +70,65 @@ class Linear(bp.Policy):
             self.log("Something Went Wrong...", 'EXCEPTION')
             self.log(e, 'EXCEPTION')
 
-    def getQValue(self, field):
-        weights = self.weights
-        features = np.zeros(4)
-        if field >= 0 and field < 6:
-            return 0, features
-        else:
-            # which food is there
-            if field == 6: idx=0
-            if field == 7: idx=1
-            if field == 8: idx=2
-            # now check if next field is free
-            if field < 0: idx=3
-            features[idx] = 1
-            q_value = weights[0, idx]  # + bias
-            return q_value, features
+    def getQValue(self, state, action):
+        board, head = state
+        head_pos, direction = head
+        # get a Position object of the position in the relevant direction from the head:
+        next_position = head_pos.move(bp.Policy.TURNS[direction][action])
+        r, c = next_position
+        field = board[r, c]
+
+        idx = field+1
+        features = self.features
+        features[idx] = 1
+        q_value = self.weights[0, idx]
+        return q_value, features
+
+        #
+        # if field >= 0 and field < 6:
+        #     return 0, features
+        # else:
+        #     # which food is there
+        #     if field == 6: idx=0
+        #     if field == 7: idx=1
+        #     if field == 8: idx=2
+        #     # now check if next field is free
+        #     if field < 0: idx=3
+        #     features[idx] = 1
+        #     q_value = weights[0, idx]  # + bias
+        #     return q_value, features
 
 
     def act(self, round, prev_state, prev_action, reward, new_state, too_slow):
 
-        board, head = new_state
-        head_pos, direction = head
-
         if np.random.rand() < self.epsilon:
             action = np.random.choice(bp.Policy.ACTIONS)
-            next_position = head_pos.move(bp.Policy.TURNS[direction][action])
-            r, c = next_position
-            q_value, features = self.getQValue(board[r, c])
+            q_max, features = self.getQValue(new_state, action)
 
         else:
             res = {'features': [],
                    'q_values': np.zeros(3),
                    'action': []}
             for dir_idx, a in enumerate(list(np.random.permutation(bp.Policy.ACTIONS))):
-                # get a Position object of the position in the relevant direction from the head:
-                next_position = head_pos.move(bp.Policy.TURNS[direction][a])
-                r, c = next_position
 
-                q_value, features_a = self.getQValue(board[r, c])  # getQValue(a, next_position, board, direction)
+                q_value, features_a = self.getQValue(new_state, a)  # getQValue(a, next_position, board, direction)
                 res['features'].append(features_a)
                 res['action'].append(a)
                 res['q_values'][dir_idx] = q_value
 
-            q_value, q_max_idx = np.max(res['q_values']), np.argmax(res['q_values'])
+            q_max, q_max_idx = np.max(res['q_values']), np.argmax(res['q_values'])
             features = res['features'][q_max_idx]
             action = res['action'][q_max_idx]
         if round <= 1:
             delta = 0
         else:
             # print(self.last_qvalues, self.last_actions, self.last_deltas)
-            delta = self.last_qvalues[-1] - (reward + (self.gamma * q_value))
-        self.last_actions.append(action)
-        self.last_qvalues.append(q_value)
+
+            prev_qvalue, prev_features = self.getQValue(prev_state, prev_action)
+            delta = prev_qvalue - (reward + (self.gamma * q_max))
+
+        # self.last_actions.append(action)
+        # self.last_qvalues.append(q_max)
         self.last_features.append(features)
         self.last_deltas.append(delta)
-        self.epsilon = self.epsilon * EPSILON_RATE
         return action
