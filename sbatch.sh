85b84168381d269c532d6853d61150f0a07f922d
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-27 19:03:51 +0200

    sbatch

--- sbatch.sh
+++ sbatch.sh
@@ -9,5 +9,5 @@
 source /cs/labs/shais/dsgissin/apml_snake/bin/activate.csh
 module load tensorflow
 
-python3 <YOUR_FOLDER>/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(lr=0.001);MyPolicy(lr=0.001)" -D 5000 -s 1000 -l "<YOUR_LOG_PATH>" -r 0 -plt 0.01 -pat 0.005 -pit 60
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.001)" -D 5000 -s 1000 -l "/logs/match_01.log" -r 0 -plt 0.01 -pat 0.005 -pit 60
 
