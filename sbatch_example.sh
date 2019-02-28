f5bd27c9ddf2acfaa533bef5c8062561b7e61181
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-28 13:54:23 +0200

    setup

--- sbatch_example.sh
+++ sbatch_example.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 #SBATCH --cpus-per-task=2
-#SBATCH --output=<YOUR_OUTPUT_PATH_HERE>
+SBATCH --output=/cs/usr/franziska/Documents/ReinforcementLearning/logs/
 #SBATCH --mem-per-cpu=500M
 #SBATCH --account=aml
 #SBATCH --constraint="sm"
@@ -12,13 +12,20 @@ module load tensorflow
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001)" \
     -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60 \
-    -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_01.log" \
-    -o "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_01.out" \
+    -l match_01.log \
+    -o match_01.out \
     -rt match_01.pickle
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)" \
     -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60 \
-    -l "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_02.log" \
-    -o "/cs/usr/franziska/Documents/ReinforcementLearning/logs/match_02.out" \
+    -l match_02.log \
+    -o match_02.out \
+    -rt match_02.pickle
+
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
+    -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)" \
+    -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60 \
+    -l match_03.log \
+    -o match_03.out \
     -rt match_02.pickle
