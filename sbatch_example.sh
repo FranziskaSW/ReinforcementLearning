1e773f9c9d11f7886dbdcd23a8dbf9f9d7ba1dbb
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-28 14:35:10 +0200

    change policy time

--- sbatch_example.sh
+++ sbatch_example.sh
@@ -11,21 +11,21 @@ module load tensorflow
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001)" \
-    -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60 \
+    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
     -l match_01.log \
     -o match_01.out \
     -rt match_01.pickle
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)" \
-    -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60 \
+    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
     -l match_02.log \
     -o match_02.out \
     -rt match_02.pickle
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(batch_size=16);MyPolicy(batch_size=32);MyPolicy(batch_size=64)" \
-    -D 7000 -s 1000 -r 0 -plt 0.01 -pat 0.005 -pit 60 \
+    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
     -l match_03.log \
     -o match_03.out \
     -rt match_02.pickle
