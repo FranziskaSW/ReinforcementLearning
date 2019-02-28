f7d55388cbe8944964d2ffabc3fcb2d2c16880db
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-28 19:11:32 +0200

    increased learning time

--- sbatch_example.sh
+++ sbatch_example.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 #SBATCH --cpus-per-task=2
-SBATCH --output=/cs/usr/franziska/Documents/ReinforcementLearning/logs/
+#SBATCH --output=/cs/usr/franziska/Documents/ReinforcementLearning/logs/
 #SBATCH --mem-per-cpu=500M
 #SBATCH --account=aml
 #SBATCH --constraint="sm"
@@ -9,32 +9,33 @@ SBATCH --output=/cs/usr/franziska/Documents/ReinforcementLearning/logs/
 source /cs/labs/shais/dsgissin/apml_snake/bin/activate.csh
 module load tensorflow
 
+
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
+    -P "Avoid(epsilon=0.1);MyPolicy(gamma=0.1);MyPolicy(gamma=0.3);MyPolicy(gamma=0.5);MyPolicy(gamma=0.7)" \
+    -D 20000 -s 2000 -r 0 -plt 0.1 -pat 0.02 -pit 60 \
+    -l match_01_ga.log \
+    -o match_01_ga.out \
+    -rt match_01_ga.pickle
+
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.1);MyPolicy(epsilon=0.1);MyPolicy(epsilon=0.2);MyPolicy(epsilon=0.3);MyPolicy(epsilon=0.4)" \
-    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_05_eps.log \
-    -o match_05_eps.out \
-    -rt match_05_eps.pickle
+    -D 20000 -s 2000 -r 0 -plt 0.1 -pat 0.02 -pit 60 \
+    -l match_01_eps.log \
+    -o match_01_eps.out \
+    -rt match_01_eps.pickle
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.1);MyPolicy(batch_size=8);MyPolicy(batch_size=16);MyPolicy(batch_size=32);MyPolicy(batch_size=64)" \
-    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_05_bs.log \
-    -o match_05_bs.out \
-    -rt match_05_bs.pickle
+    -D 20000 -s 2000 -r 0 -plt 0.1 -pat 0.02 -pit 60 \
+    -l match_01_bs.log \
+    -o match_01_bs.out \
+    -rt match_01_bs.pickle
 
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.1);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001);MyPolicy(learning_rate=0.00001)" \
-    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_05_lr.log \
-    -o match_05_lr.out \
-    -rt match_05_lr.pickle
+    -D 20000 -s 2000 -r 0 -plt 0.1 -pat 0.02 -pit 60 \
+    -l match_01_lr.log \
+    -o match_01_lr.out \
+    -rt match_01_lr.pickle
 
-
-python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
-    -P "Avoid(epsilon=0.1);MyPolicy(gamma=0.1);MyPolicy(gamma=0.3);MyPolicy(gamma=0.5);MyPolicy(gamma=0.7)" \
-    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_05_ga.log \
-    -o match_05_ga.out \
-    -rt match_05_ga.pickle
