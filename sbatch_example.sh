99b6173bfdeee2294fb6c78eaed76b8ae541f563
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-28 15:48:10 +0200

    setup match_04

--- sbatch_example.sh
+++ sbatch_example.sh
@@ -10,23 +10,31 @@ source /cs/labs/shais/dsgissin/apml_snake/bin/activate.csh
 module load tensorflow
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
-    -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(epsilon=0.4);MyPolicy(epsilon=0.5);MyPolicy(epsilon=0.6)" \
+    -P "Avoid(epsilon=0.1);MyPolicy(epsilon=0.4);MyPolicy(epsilon=0.5);MyPolicy(epsilon=0.6);MyPolicy(epsilon=0.7)" \
     -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_02.log \
-    -o match_02.out \
-    -rt match_02.pickle
+    -l match_04_eps.log \
+    -o match_04_eps.out \
+    -rt match_04_eps.pickle
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
-    -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(batch_size=16);MyPolicy(batch_size=32);MyPolicy(batch_size=64)" \
+    -P "Avoid(epsilon=0.1);MyPolicy(batch_size=8);MyPolicy(batch_size=16);MyPolicy(batch_size=32);MyPolicy(batch_size=64)" \
     -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_03.log \
-    -o match_03.out \
-    -rt match_02.pickle
+    -l match_04_bs.log \
+    -o match_04_bs.out \
+    -rt match_04_bs.pickle
 
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
-    -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001)" \
+    -P "Avoid(epsilon=0.1);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001);MyPolicy(learning_rate=0.0001)" \
     -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_01.log \
-    -o match_01.out \
-    -rt match_01.pickle
+    -l match_04_lr.log \
+    -o match_04_lr.out \
+    -rt match_04_lr.pickle
+
+
+python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
+    -P "Avoid(epsilon=0.1);MyPolicy(gamma=0.1);MyPolicy(gamma=0.3);MyPolicy(gamma=0.5);MyPolicy(gamma=0.7)" \
+    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
+    -l match_04_ga.log \
+    -o match_04_ga.out \
+    -rt match_04_ga.pickle
