72d52ca8c643e093bac266389e2b0ba7e68df988
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-28 19:58:39 +0200

    increased learning time

--- sbatch_example.sh
+++ sbatch_example.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 #SBATCH --cpus-per-task=2
-#SBATCH --output=/cs/usr/franziska/ReinforcementLearning/logs/
+#SBATCH --output=/cs/usr/franziska/ReinforcementLearning/logs
 #SBATCH --mem-per-cpu=500M
 #SBATCH --account=aml
 #SBATCH --constraint="sm"
