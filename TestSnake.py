eff9faccc7e189faf978ffb796d448a68c19ac55
FranziskaSW <franziska.wehrmann@gmail.com>
2019-02-28 16:30:27 +0200

    setup TestSnake

--- TestSnake.py
+++ TestSnake.py
@@ -789,7 +789,7 @@ if __name__ == '__main__':
         players.append('Avoid(epsilon=0.1)')
 
     # create our agent:
-    params = {'epsilon': [0.4, 0.5, 0.6, 0.7,
+    params = {'epsilon': [0.4, 0.5, 0.6, 0.7],
               'gamma': [0.1, 0.3, 0.5, 0.7, 0.9],
               'learning_rate': [0.01, 0.001, 0.0001, 0.00001],
               'batch_size': [int(8), int(16), int(32), int(64), int(128)]
--- sbatch_example.sh
+++ sbatch_example.sh
@@ -11,30 +11,30 @@ module load tensorflow
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.1);MyPolicy(epsilon=0.4);MyPolicy(epsilon=0.5);MyPolicy(epsilon=0.6);MyPolicy(epsilon=0.7)" \
-    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_04_eps.log \
-    -o match_04_eps.out \
-    -rt match_04_eps.pickle
+    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
+    -l match_05_eps.log \
+    -o match_05_eps.out \
+    -rt match_05_eps.pickle
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.1);MyPolicy(batch_size=8);MyPolicy(batch_size=16);MyPolicy(batch_size=32);MyPolicy(batch_size=64)" \
-    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_04_bs.log \
-    -o match_04_bs.out \
-    -rt match_04_bs.pickle
+    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
+    -l match_05_bs.log \
+    -o match_05_bs.out \
+    -rt match_05_bs.pickle
 
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.1);MyPolicy(learning_rate=0.01);MyPolicy(learning_rate=0.001);MyPolicy(learning_rate=0.0001);MyPolicy(learning_rate=0.0001)" \
-    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_04_lr.log \
-    -o match_04_lr.out \
-    -rt match_04_lr.pickle
+    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
+    -l match_05_lr.log \
+    -o match_05_lr.out \
+    -rt match_05_lr.pickle
 
 
 python3 /cs/usr/franziska/Documents/ReinforcementLearning/Snake.py \
     -P "Avoid(epsilon=0.1);MyPolicy(gamma=0.1);MyPolicy(gamma=0.3);MyPolicy(gamma=0.5);MyPolicy(gamma=0.7)" \
-    -D 7000 -s 1000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
-    -l match_04_ga.log \
-    -o match_04_ga.out \
-    -rt match_04_ga.pickle
+    -D 20000 -s 2000 -r 0 -plt 0.05 -pat 0.01 -pit 60 \
+    -l match_05_ga.log \
+    -o match_05_ga.out \
+    -rt match_05_ga.pickle
